4cf80020c2e4f5f1f06201cefff9d2fc7192a0c3
kingssik <hyunsikkang25@gmail.com>
2023-09-03 15:17:56 +0900

    게시글 상세 페이지 구현(날짜 아직)

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -53,6 +53,31 @@ public class Main {
           }
         }
 
+      } else if (cmd.startsWith("article detail ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);  // "2" -> 2
+
+        Article foundArticle = null;
+
+        for (int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+
+          if (article.id == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
+          continue;
+        }
+
+        System.out.printf("번호 : %d\n", foundArticle.id);
+        System.out.printf("날짜 : 2023-12-09 12:12:12\n");
+        System.out.printf("제목 : %s\n", foundArticle.title);
+        System.out.printf("내용 : %s\n", foundArticle.body);
+
       } else {
         System.out.println("존재하지 않는 명령어입니다");
       }
