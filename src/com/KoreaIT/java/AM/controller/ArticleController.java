33cf6eb43cb73ae3338f20fb739ed0c031e665fc
kingssik <hyunsikkang25@gmail.com>
2023-09-17 15:16:47 +0900

    게시글 작성자만 수정/삭제 가능하게 처리

--- src/com/KoreaIT/java/AM/controller/ArticleController.java
+++ src/com/KoreaIT/java/AM/controller/ArticleController.java
@@ -128,6 +128,11 @@ public class ArticleController extends Controller {
       return;
     }
 
+    if (foundArticle.memberId != loginedMember.id) {
+      System.out.println("권한이 없습니다.");
+      return;
+    }
+
     System.out.printf("제목 : ");
     String title = sc.nextLine();
     System.out.printf("내용 : ");
@@ -143,14 +148,19 @@ public class ArticleController extends Controller {
     String[] cmdBits = cmd.split(" ");
     int id = Integer.parseInt(cmdBits[2]);
 
-    int foundIndex = getArticleIndexById(id);
+    Article foundArticle = getArticleById(id);
 
-    if (foundIndex == -1) {
+    if (foundArticle == null) {
       System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
       return;
     }
 
-    articles.remove(foundIndex);
+    if (foundArticle.memberId != loginedMember.id) {
+      System.out.println("권한이 없습니다.");
+      return;
+    }
+
+    articles.remove(foundArticle);
     System.out.printf("%d번 게시물이 삭제 되었습니다\n", id);
   }
 
